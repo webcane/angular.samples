3ad0dd89129e4a477a2e2c4e8a52a1f74c374001
Mikhail Niedre <webcane@ya.ru>
2017-03-02 23:11:15 +0300

    init

---
+++ angular-ui-routing/100 init/app.js
@@ -0,0 +1,21 @@
+(function(angular) {
+  'use strict';
+  var app = angular.module('app', ['ui.router']);
+  app.config(config);
+  // routing
+  config.$inject = ['$stateProvider'];
+  function config($stateProvider) {
+    var helloState = {
+      name: 'hello',
+      url: '/hello',
+      template: '<h3>hello world!</h3>'
+    }
+    var aboutState = {
+      name: 'about',
+      url: '/about',
+      template: '<h3>It's about the UI-Router!</h3>'
+    }
+    $stateProvider.state(helloState);
+    $stateProvider.state(aboutState);
+  };
+})(angular);
