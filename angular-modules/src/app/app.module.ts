12ad3c759162add7399112382dbc70cf82b9e179
Mikhail Niedre <mikhail.niedre@t-systems.com>
2020-11-12 20:29:47 +0300

    add shared header component

--- angular-modules/src/app/app.module.ts
+++ angular-modules/src/app/app.module.ts
@@ -4,15 +4,19 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeModule} from './home/home.module';
+import {HeaderComponent} from './shared/layout/header/header.component';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HomeModule
+    SharedModule,
+    HomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-modules/src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * Is shared component, but do not belong shared module
+ */
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
